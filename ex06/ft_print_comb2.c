b95188238412db60fddd78a10d988c08116fc154
harumoren2507 <kuroshiro0725@gmail.com>
2024-01-09 23:47:04 +0900

    nendawa2034

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: riyam@student.42tokyo.jp  <riyam@studen    +#+  +:+       +#+        */
+/*   By: rtoriyam <kuroshiro0725@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 15:55:15 by rtoriyam          #+#    #+#             */
-/*   Updated: 2024/01/03 17:30:22 by riyam@stude      ###   ########.fr       */
+/*   Updated: 2024/01/09 23:43:44 by rtoriyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void	ft_putchar(char c)
+void ft_putchar(char c)
 {
-	write(1, &c, 1);
+	write (1, &c, 1);
 }
 
 void	ft_print_comb2(void)
 {
 	int	i;
+
 	int	j;
 
 	i = 0;
@@ -30,20 +30,20 @@ void	ft_print_comb2(void)
 		{
 			ft_putchar('0' + i / 10);
 			ft_putchar('0' + i % 10);
-			write(1, " ", 1);
+			write (1, " ", 1);
 			ft_putchar('0' + j / 10);
 			ft_putchar('0' + j % 10);
-			if (!(i == 98 && j == 99))
-				write(1, ", ", 2);
+			if (!(i == 98))
+				write (1, ", ", 2);
 			j++;
 		}
 		i++;
 	}
 }
 
-	int	main(void)
-	{
-	ft_print_comb2(); 
-	write(1, "\n", 1);
-	return (0);
-	}
+int main(void)
+{
+	ft_print_comb2();
+	write (1, "\n", 1);
+	return 0;
+}
