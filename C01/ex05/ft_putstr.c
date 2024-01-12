2ceccef5cc292df2c9811244212d69580cc53b74
harumoren2507 <kuroshiro0725@gmail.com>
2024-01-12 18:59:11 +0900

    C01_2

---
+++ C01/ex05/ft_putstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: harumoren2507 <kuroshiro0725@gmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 18:05:26 by harumoren25       #+#    #+#             */
+/*   Updated: 2024/01/12 18:46:49 by harumoren25      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, &*str, 1);
+		str++;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		ft_putchar(*(str + i));
+		i++;
+	}
+}
