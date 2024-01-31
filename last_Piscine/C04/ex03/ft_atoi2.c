3d1ac9bdffe86773d060c3fd59d144e03cda7a63
harumoren2507 <kuroshiro0725@gmail.com>
2024-01-31 22:20:55 +0900

    minimum_minimum

--- last_Piscine/C04/ex03/ft_atoi2.c
+++ last_Piscine/C04/ex03/ft_atoi2.c
@@ -6,9 +6,11 @@
 /*   By: harumoren2507 <kuroshiro0725@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 20:11:39 by harumoren25       #+#    #+#             */
-/*   Updated: 2024/01/30 18:49:43 by harumoren25      ###   ########.fr       */
+/*   Updated: 2024/01/31 22:20:10 by harumoren25      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int ft_atoi(char *str)
 {
@@ -28,12 +30,25 @@ int ft_atoi(char *str)
         }
         str++;
     }
-    while ((0 <= *str) && (*str <= 9))
+    while (('0' <= *str) && (*str <= '9'))
     {
-        result = 
+        result = result * 10 + (*str - '0');
+        str++;
     }
+    return (sign * result);
 }
 
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        printf("%s\n", argv[0]);
+        return 1;
+    }
+    int result = ft_atoi(argv[1]);
+    printf("%d\n", result);
+    return 0;
+}
 
 
 
