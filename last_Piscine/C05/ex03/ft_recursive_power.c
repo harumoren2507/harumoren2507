f0f24cf9d10011d713869a905a1bddf9404c63be
harumoren2507 <kuroshiro0725@gmail.com>
2024-02-07 21:50:04 +0900

    minimum

--- last_Piscine/C05/ex03/ft_recursive_power.c
+++ last_Piscine/C05/ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: harumoren2507 <kuroshiro0725@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 21:28:47 by harumoren25       #+#    #+#             */
-/*   Updated: 2024/02/07 21:35:45 by harumoren25      ###   ########.fr       */
+/*   Updated: 2024/02/07 21:49:46 by harumoren25      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,35 @@ int main(void)
     printf("%d\n", ft_recursive_power(3, 2));
     return 0;
 }
+
+/*
+*ft_recursive_power(3, 3) 　の場合
+
+ft_recursive_power(3, 3 - 1) *3
+
+*ft_recursive_power(3, 2)を呼び出す
+
+ft_recursive_power(3, 2 - 1) *3
+
+*ft_recursive_power(3, 1) を呼び出す
+
+ft_recursive_power(3, 1 - 1) *3
+
+*ft_recursive_power(3, 0) を呼び出す
+
+*ft_recursive_power(3, 0)　= 1
+返り値としてreturn 1 が返される　ここから関数がバックステップしていく
+
+*ft_recursive_power(3, 1) は　3を返す
+= ft_recursive_power(3, 0) *3
+ = 1 * 3 = 3
+
+*ft_recursive_power(3, 2)は 9を返す
+= ft_recursive_power(3, 1) *3
+ = 3 * 3 = 9
+
+*ft_recursive_power(3, 3) は27を返す
+ft_recursive_power(3, 2) *3
+ = 9 * 3 = 27
+
+*/
