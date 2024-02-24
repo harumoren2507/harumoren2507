b34a313701da14db3273f174cfb7d95d757dcff1
harumoren2507 <kuroshiro0725@gmail.com>
2024-02-24 23:33:34 +0900

    fizzbuzz

--- recursion/m_controlflow/fizzbuzz.cpp
+++ recursion/m_controlflow/fizzbuzz.cpp
@@ -25,29 +25,15 @@ void    fizzBuzz(int n)
     while (current <= n)
     {
         if (current % 3 == 0 && current % 5 == 0)
-        {
-        write(1, "FizzBuzz", 9);
-            if (current != n)
-                ft_putchar('-');
-        }
+            write(1, "FizzBuzz", 8);
         else if (current % 3 == 0)
-        {
-            write(1, "FIZZ", 5);
-            if (current != n)
-                ft_putchar('-');
-        }
+            write(1, "FIZZ", 4);
         else if (current % 5 == 0)
-        {
-            write(1, "Buzz", 5);
-            if (current != n)
-                ft_putchar('-');
-        }
+            write(1, "Buzz", 4);
         else
-        {
             ft_putnbr_E(current);
-            if (current != n)
-                ft_putchar('-');
-        }
+        if (current != n)
+            ft_putchar('-');
         current++;
     }
     write(1, "\n", 1);
@@ -56,7 +42,7 @@ void    fizzBuzz(int n)
 
 int main(void)
 {
-    int nb = 5;
+    int nb = 125;
     fizzBuzz(nb);
     return 0;
 }
